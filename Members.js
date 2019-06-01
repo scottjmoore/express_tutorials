b1ec54a96c41bc5238d142de101820c61789e78d
Scott Moore <scott.moore@scottmooresoftware.com>
2019-06-01 22:10:50 +0100

    Add more stuff...

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+        email: 'jane@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Scott Moore',
+        email: 'scott.moore@scottmooresoftware.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
